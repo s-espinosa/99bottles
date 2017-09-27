9a75b83c8e1d54c40c5fced76ed1497c2839cafa
Sal Espinosa <sespinos@gmail.com>
2017-09-27 09:07:56 -0600

    Make single case conform to others

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -5,7 +5,7 @@ class Bottles
     elsif number == 2
       "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number - 1} bottle of beer on the wall.\n"
     elsif number == 1
-      "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+      "#{number} bottle of beer on the wall, #{number} bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
     else
       "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
     end
